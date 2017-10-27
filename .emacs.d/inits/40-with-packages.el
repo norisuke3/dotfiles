30417cacf166cccd978e599161fb7caf0281d979
Noriaki Hamamoto <norisuke3@gmail.com>
2017-10-27 10:49:07 -0700

    modified yasnippet for anything interface.

--- .emacs.d/inits/40-with-packages.el
+++ .emacs.d/inits/40-with-packages.el
@@ -38,7 +38,23 @@
              ((kbd "C-x i i") . yas-insert-snippet)
              ((kbd "C-x i n") . yas-new-snippet)
              ((kbd "C-x i v") . yas-visit-snippet-file))
-  )
+  ;; anything-c-yasnippetを使わずにyasnippetをanythingインタフェースで選択する
+  ;; http://memo.sugyan.com/entry/20120111/1326288445
+  ;; https://github.com/sugyan/dotfiles/blob/master/.emacs.d/inits/04-yasnippet.el
+  (defun my-yas/prompt (prompt choices &optional display-fn)
+    (let* ((names (loop for choice in choices
+                        collect (or (and display-fn (funcall display-fn choice))
+                                    choice)))
+           (selected (anything-other-buffer
+                      `(((name . ,(format "%s" prompt))
+                         (candidates . names)
+                         (action . (("Insert snippet" . (lambda (arg) arg))))))
+                      "*anything yas/prompt*")))
+      (if selected
+          (let ((n (position selected names :test 'equal)))
+            (nth n choices))
+        (signal 'quit "user quit!"))))
+  (custom-set-variables '(yas/prompt-functions '(my-yas/prompt))))
 
 (use-package auto-complete
   :commands auto-complete
