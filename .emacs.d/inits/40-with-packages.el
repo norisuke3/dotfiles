eba3257ca58bf7a586c386fc33ea53a43308957a
Noriaki Hamamoto <norisuke3@gmail.com>
2017-10-28 06:33:59 -0700

    commented out anything keybind.

--- .emacs.d/inits/40-with-packages.el
+++ .emacs.d/inits/40-with-packages.el
@@ -1,7 +1,7 @@
 ;; use-packageで可読性の高いinit.elを書く
 ;; https://qiita.com/kai2nenobu/items/5dfae3767514584f5220
-(use-package anything-startup
-  :bind (("C-l" . anything-for-files)))
+;; (use-package anything-startup
+;;   :bind (("C-l" . anything-for-files)))
 
 ;; #11 Emacs に革命を起こすパッケージ「helm」
 ;; http://emacs.rubikitch.com/sd1503-helm/ (Software Design 2015年3月号掲載記事)
