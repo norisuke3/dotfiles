7b9e5675eb73f5b9feefee1a88b45eb4c4cbfef0
Noriaki Hamamoto <norisuke3@gmail.com>
2020-11-03 10:28:16 -0800

    added orgmode.org to the package archives.

--- .emacs.d/inits/20-install-packages.el
+++ .emacs.d/inits/20-install-packages.el
@@ -4,6 +4,7 @@
 
 (when (require 'package nil t)
   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
   (package-initialize))
 
 (defvar installing-package-list
