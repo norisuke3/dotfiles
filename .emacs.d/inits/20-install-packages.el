96b828b093e66c5b80648da30259dd997fe15f36
Noriaki Hamamoto <norisuke3@gmail.com>
2021-06-06 17:25:43 -0700

    removed unnecessary declaration of the default package archive since it's already in the list.

--- .emacs.d/inits/20-install-packages.el
+++ .emacs.d/inits/20-install-packages.el
@@ -5,7 +5,6 @@
 (when (require 'package nil t)
   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages//") t)
   (package-initialize))
 
 (defvar installing-package-list
