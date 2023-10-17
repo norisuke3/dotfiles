3296be27e83b26d1d5ccad7767d66f12b41d4d4e
Noriaki Hamamoto <norisuke3@gmail.com>
2023-10-17 15:15:07 -0700

    added a setup for Emacs Application Framework.

--- .emacs.d/inits/40-with-packages.el
+++ .emacs.d/inits/40-with-packages.el
@@ -538,3 +538,21 @@
     "Open GitHub Gist page with chrome."
     (interactive)
     (browse-url "https://gist.github.com/norisuke3")))
+
+;; EAF (Emacs Application Framework)
+;; https://github.com/emacs-eaf/emacs-application-framework
+(use-package eaf
+  :load-path "~/.emacs.d/emacs-application-framework"
+  :custom
+  ; See https://github.com/emacs-eaf/emacs-application-framework/wiki/Customization
+  (eaf-browser-continue-where-left-off t)
+  (eaf-browser-enable-adblocker t)
+  (browse-url-browser-function 'eaf-open-browser)
+  :config
+  (require 'eaf-browser)
+  (require 'eaf-pdf-viewer)
+  (defalias 'browse-web #'eaf-open-browser)
+  (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
+  (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
+  ;; (eaf-bind-key take_photo "p" eaf-camera-keybinding)
+  (eaf-bind-key nil "M-q" eaf-browser-keybinding)) ;; unbind, see more in the Wiki
